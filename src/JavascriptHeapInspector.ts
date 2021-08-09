e25ccda81aab4b18aa4118c6e7ec70a756376f90
JonLuca DeCaro <jdecaro@usc.edu>
2021-08-08 23:18:57 -0400

    working graph

--- src/JavascriptHeapInspector.ts
+++ src/JavascriptHeapInspector.ts
@@ -1,13 +1,17 @@
 import { HeapSnapshotLoader } from "./lib/HeapSnapshotLoader";
 import * as fs from "fs";
 import * as path from "path";
-import { Node, Edge } from "./lib/HeapSnapshotModel";
+import { Node, Edge, NodeFilter } from "./lib/HeapSnapshotModel";
 import { HeapSnapshotObjectNode } from "./lib/HeapSnapshotGridNodes";
 import { JSHeapSnapshot } from "./lib/HeapSnapshot";
 
 class JavascriptHeapInspector {
   data: any;
   snapshot?: JSHeapSnapshot;
+  _filterInProgress?: NodeFilter | null;
+  _nextRequestedFilter?: NodeFilter | null;
+  _lastFilter?: NodeFilter | null;
+
   constructor(data: any) {
     this.data = data;
   }
@@ -22,8 +26,29 @@ class JavascriptHeapInspector {
     const node = new Node(-1, "root", 0, snapshot.rootNodeIndex, 0, 0, "");
     const fakeEdge = new Edge("", node, "", -1);
     const hsON = new HeapSnapshotObjectNode(this, snapshot, fakeEdge, null);
+    this._populateChildren();
     console.log(staticData);
   };
+
+  async _populateChildren(maybeNodeFilter?: NodeFilter): Promise<void> {
+    const nodeFilter = maybeNodeFilter || new NodeFilter();
+
+    if (this._filterInProgress) {
+      this._nextRequestedFilter = this._filterInProgress.equals(nodeFilter)
+        ? null
+        : nodeFilter;
+      return;
+    }
+    if (this._lastFilter && this._lastFilter.equals(nodeFilter)) {
+      return;
+    }
+    this._filterInProgress = nodeFilter;
+
+    if (this.snapshot) {
+      const aggregates = await this.snapshot.aggregatesWithFilter(nodeFilter);
+      console.log(aggregates);
+    }
+  }
 }
 const data = fs.readFileSync(
   path.join(__dirname, "../sample.heapsnapshot"),
